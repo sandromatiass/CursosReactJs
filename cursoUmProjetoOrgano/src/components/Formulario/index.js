e8a164effa44210b75bbececea130ccabe0c5e49
Sandro Matias <devsandromatias@gmail.com>
2023-04-21 09:15:00 -0300

    Aprendendo a fazer listas

--- cursoUmProjetoOrgano/src/components/Formulario/index.js
+++ cursoUmProjetoOrgano/src/components/Formulario/index.js
@@ -1,14 +1,67 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+  
+  const conferencias = ["Conferêcia Leste", "Conferêcia Oeste"];
+
+  const divisao = [
+    "Divisão Atlântico",
+    "Divisão Central",
+    "Divisão Sudeste",
+    "Divisão Noroeste",
+    "Divisão Pacífico",
+    "Divisão Sudoeste"
+  ];
+    
+  const times = [
+    "Boston Celtics",
+    "Brooklyn Nets",
+    "New York Knicks",
+    "Philadelphia 76ers",
+    "Toronto Raptors",
+    "Chicago Bulls",
+    "Cleveland Cavaliers",
+    "Detroit Pistons",
+    "Indiana Pacers",
+    "Milwaukee Bucks",
+    "Atlanta Hawks",
+    "Charlotte Hornets",
+    "Miami Heat",
+    "Orlando Magic",
+    "Washington Wizards",
+    "Denver Nuggets",
+    "Minnesota Timberwolves",
+    "Portland Trail Blazers",
+    "Oklahoma City Thunder",
+    "Utah Jazz",
+    "Golden State Warriors",
+    "Los Angeles Clippers",
+    "Los Angeles Lakers",
+    "Phoenix Suns",
+    "Sacramento Kings",
+    "Dallas Mavericks",
+    "Houston Rockets",
+    "Memphis Grizzlies",
+    "New Orleans Pelicans",
+    "San Antonio Spurs",
+  ];
+
   return (
     <section className="formulario">
       <form>
         <h2>Preencha os dados para criar o card do seu personagem de anime</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Anexar imagem" />
+        <CampoTexto label="Jogador" placeholder="Digite o nome do jogador" />
+        <CampoTexto label="Posição" placeholder="Digite a posição" />
+        <CampoTexto label="Imagem" placeholder="Anexar imagem do jogador" />
+        <ListaSuspensa label="Conferência" itens={conferencias} />
+        <ListaSuspensa label="Divisão" itens={divisao} />
+        <ListaSuspensa label="Time" itens={times} />
+        <Botao> 
+          Criar Card
+        </Botao>
       </form>
     </section>
   );
