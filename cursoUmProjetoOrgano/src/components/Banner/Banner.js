821c4421886d9e6532cd3bed6129c9ee2baa95a8
Sandro Matias <devsandromatias@gmail.com>
2023-04-23 09:11:51 -0300

    corrigindo bug

--- cursoUmProjetoOrgano/src/components/Banner/Banner.js
+++ cursoUmProjetoOrgano/src/components/Banner/Banner.js
@@ -1,10 +1,15 @@
 import './Banner.css'
 
 export const Banner = () => {
-    return(
+    return (
+      <>
         <header className="banner">
-            <img src="/imagens/banner.png" alt="O banner principal da página do Organo."/>
-        </header> 
-    )
+          <img
+            src="../imagens/banner.png"
+            alt="O banner principal da página do Organo."
+          />
+        </header>
+      </>
+    );
 }
 
